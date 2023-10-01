b915166789e9008c48b19dd138b8b70115128fdd
Akiranya <porridgen@gmail.com>
2023-10-01 23:35:09 +0800

    chore: prepare for team collaboration

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+subprojects {
+    apply(plugin = "cc.mewcraft.repo-conventions")
+    apply(plugin = "cc.mewcraft.java-conventions")
+
+    group = "cc.mewcraft.adventurelevel"
+    version = "1.1.1"
+    description = "Add adventure level to players"
+}
